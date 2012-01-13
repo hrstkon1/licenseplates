d3a2d06b422380cbe740de0393f469e4d5a164a2
James Pritts <jbpritts@gmail.com>
2012-01-13 19:01:19 +0100

    added files

--- matlab/lic_detect.m
+++ matlab/lic_detect.m
@@ -1,21 +1,17 @@
-function sj_labels = lic_detect(lic_num)
+function sj_labels = lic_detect(lic_num,debug_output)
+global m n;
 
 if nargin < 1
     lic_num = 1;
 end
 
+if nargin < 2
+    debug_output = 0;
+end
+
 load('../data/np-images-5000.mat');
 
 [uf,sf,ub,sb,alpha,labels,B] = lic_get_emission_pd();
-
-figure;
-ix = uf-3*sf:1e-3:uf+3*sf; %covers more than 99% of the curve
-iy = pdf('normal',ix,uf,sf);
-figure;plot(ix,iy);
-
-ix = ub-3*sb:1e-3:ub+3*sb; %covers more than 99% of the curve
-iy = pdf('normal',ix,ub,sb);
-hold on;plot(ix,iy);hold off;
 
 x = cropInputImage(data(lic_num).nimg);
 xs = sort(unique(x));
@@ -30,52 +26,51 @@ psi = zeros(m,n);
 rho = zeros(m,n);
 
 p_Xj_Sj = zeros(m,n);
+P_Xj_Sj = cell(1,n);
 
 for j = 1:n
     p_XijF = repmat(normcdf(x(:,j)+dx,uf,sf)-normcdf(x(:,j)-dx,uf,sf),1,m);
     p_XijB = repmat(normcdf(x(:,j)+dx,ub,sb)-normcdf(x(:,j)-dx,ub,sb),1,m);
     p_Xj_Sj(:,j) = prod(alpha.*p_XijF+(1-alpha).*p_XijB,1);
+    P_Xj_Sj{j} = lic_make_emissions_mtx(labels,p_Xj_Sj(:,j));
 end
 
 rho(:,1) = [p_Xj_Sj(1,1) zeros(1,m-1)]';
 rho(:,1) = rho(:,1)/sum(rho(:,1));
 for j = 2:n
-    P_Xj_Sj = lic_make_emissions_mtx(labels,p_Xj_Sj(:,j)');
-    P2 = P.*P_Xj_Sj;
+    P2 = P_Xj_Sj{j}*P;
     rho(:,j) = P2*rho(:,j-1);
     rho(:,j) = rho(:,j)/sum(rho(:,j));
 end
 
-figure;imagesc(rho);colormap(gray);
 
-P_Xj_Sj = lic_make_emissions_mtx(labels,p_Xj_Sj(:,end)');
-P2 = P.*P_Xj_Sj;
+P2 = P_Xj_Sj{n}*P;
 
 psi(:,end) = P2'*ones(m,1);
 psi(:,end) = psi(:,end)/sum(psi(:,end));
 for j = n-1:-1:1
-    P_Xj_Sj = lic_make_emissions_mtx(labels,p_Xj_Sj(:,j)');
-    P2 = P.*P_Xj_Sj;
+    P2 = P_Xj_Sj{j}*P;
     psi(:,j) = P2'*psi(:,j+1);
     psi(:,j) = psi(:,j)/sum(psi(:,j));
 end
 
 p_xs = rho.*psi;
 
-[p_sj,sj] = max(p_xs,[],1);
-figure;imagesc(p_xs);colormap gray;
+sj = lic_calc_best_path(p_xs,P);
+sj_labels = lic_get_glyph_col(sj,labels,B);
 
-ix = sum(repmat(sj,size(labels,2),1) > repmat(labels',1,numel(sj)))+1;
-ch = B(ix);
-labels2 = [0 labels];
-col = sj-labels2(ix);
+if debug_output
+    figure;
+    ix = uf-3*sf:1e-3:uf+3*sf; %covers more than 99% of the curve
+    iy = pdf('normal',ix,uf,sf);
+    figure;plot(ix,iy);
 
-for i = 1:length(ch)
-    if ch(i) ~= '#'
-        sj_labels{i} = sprintf('%s%d',ch(i),col(i));
-    else
-        sj_labels{i} = '#';
-    end
+    ix = ub-3*sb:1e-3:ub+3*sb; %covers more than 99% of the curve
+    iy = pdf('normal',ix,ub,sb);
+    hold on;plot(ix,iy);hold off;
+
+    figure;imagesc(rho);colormap(gray);
+    figure;imagesc(p_xs);colormap gray;
 end
 
 figure;
--- matlab/lic_make_emissions_mtx.m
+++ matlab/lic_make_emissions_mtx.m
@@ -1,34 +1,3 @@
 function PX_SJ = lic_make_emissions_mtx(labels,px_sj)
-m = numel(labels);
-n = labels(end);
-
-p_same = 0.1;
-p_next = 1.0-p_same;
-
-ii = [];
-jj = [];
-s = [];
-
-% from whitespace 
-ii = cat(2,ii,[1 labels(1:end-1)+1]);
-jj = cat(2,jj,ones(size(ii)));
-s = px_sj(ii);
-
-% from glyph columns
-for i = 2:m
-    k = labels(i)-labels(i-1);
-    ii_tmp = [labels(i-1)+1:labels(i) labels(i-1)+2:labels(i)];
-    ii = cat(2,ii,ii_tmp);
-    jj = cat(2,jj,...
-             [labels(i-1)+1:labels(i) labels(i-1)+1:labels(i)-1]);
-    s = cat(2,s,px_sj(ii_tmp));
-end
-
-% add special case for terminal glyph columns
-k = m-1;
-ii_tmp = ones(1,k);
-ii = cat(2,ii,ii_tmp);
-jj = cat(2,jj,labels(2:end));
-s = cat(2,s,px_sj(ii_tmp));
-
-PX_SJ = sparse(ii,jj,s,n,n);
+k = numel(px_sj);
+PX_SJ = spdiags(px_sj,0,k,k);
