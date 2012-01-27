83c6651a771eb5affff92f656f8a9381f5060cf4
Ondrej Hrstka <ondrej.hrstka@agents.fel.cvut.cz>
2012-01-27 17:45:16 +0100

    results generator fixed

--- matlab/lic_generate_results.m
+++ matlab/lic_generate_results.m
@@ -1,11 +1,11 @@
 function lic_generate_results(output_file_name)
     matlabpool open
-    data = cell(5000,1);
+    results = cell(5000,1);
     parfor i = 1:5000
         fprintf('%d/%d\n', i, 5000);
-        data{i} = get_label_string(lic_detect(i));
+        results{i} = lic_detect(i);
     end
-    save(output_file_name, 'data')
+    save(output_file_name, 'results')
     matlabpool close
 end
 
